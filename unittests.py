85b3fa674acea25f7734019a43c43850e59620c1
ekristina <panacejja@gmail.com>
2018-03-18 17:47:12 -0400

    test for different routes given maximum distance

--- unittests.py
+++ unittests.py
@@ -28,107 +28,107 @@ class BaseTestCase(unittest.TestCase):
         }
 
 
-# class TestRouteDistance(BaseTestCase):
-#
-#     """Unittests to test distance calculations between nodes"""
-#
-#     def test_distance_abc(self):
-#         """The distance of the route A-B-C."""
-#         self.assertEqual(
-#             RouteGraph(routes=self.graph).count_full_distance([self.a, self.b,
-#                                                                self.c]),
-#             9
-#         )
-#
-#     def test_distance_ad(self):
-#         """The distance of the route A-D."""
-#         self.assertEqual(
-#             RouteGraph(routes=self.graph).count_full_distance([self.a,
-#                                                                self.d]),
-#             5
-#         )
-#
-#     def test_distance_adc(self):
-#         """The distance of the route A-D-C."""
-#         self.assertEqual(
-#             RouteGraph(routes=self.graph).count_full_distance((self.a, self.d,
-#                                                                self.c)),
-#             13
-#         )
-#
-#     def test_distance_aebcd(self):
-#         """The distance of the route A-E-B-C-D."""
-#         self.assertEqual(
-#             RouteGraph(routes=self.graph).count_full_distance((self.a, self.e,
-#                                                                self.b, self.c,
-#                                                                self.d)),
-#             22
-#         )
-#
-#     def test_distance_aed(self):
-#         """The distance of the route A-E-D."""
-#         self.assertEqual(
-#             RouteGraph(routes=self.graph).count_full_distance((self.a, self.e,
-#                                                                self.d)),
-#             "NO SUCH ROUTE"
-#         )
-#
-#
-# class TestNumberOfTripsWithStops(BaseTestCase):
-#
-#     """Unittests for counting number of trips with
-#         given max or exact number of stops"""
-#
-#     def test_number_of_trips_c_to_c_max(self):
-#         """The number of trips starting at C and ending at C
-#         with a maximum of 3 stops.
-#         In the sample data below, there are two such trips: C-D-C (2
-#             stops). and C-E-B-C (3 stops)."""
-#         self.assertEqual(
-#             RouteGraph(routes=self.graph).count_trips_max_stops(
-#                 self.c, self.c, max_stops=3
-#             ),
-#             2
-#         )
-#
-#     def test_number_of_trips_a_to_c_exact(self):
-#         """The number of trips starting at A and ending at C
-#         with exactly 4 stops.
-#             In the sample data below, there are three such trips:
-#             A to C (via B,C,D); A
-#             to C (via D,C,D); and A to C (via D,E,B)."""
-#         self.assertEqual(
-#             RouteGraph(routes=self.graph).count_trips_fixed_stops(
-#                 self.a, self.c, fixed_stops=4
-#             ),
-#             3
-#         )
-#
-#
-# class TestShortestRoute(BaseTestCase):
-#
-#     """Unittests for calculating the shortest
-#         route in terms of distance between two points"""
-#
-#     def test_AC(self):
-#         """The length of the shortest route (in terms of distance to travel)
-#         from A to C"""
-#         self.assertEqual(
-#             RouteGraph(routes=self.graph).shortest_route(
-#                 self.a, self.c
-#             ),
-#             9
-#         )
-#
-#     def test_BB(self):
-#         """The length of the shortest route (in terms of distance to travel)
-#         from B to B"""
-#         self.assertEqual(
-#             RouteGraph(routes=self.graph).shortest_route(
-#                 self.b, self.b
-#             ),
-#             9
-#         )
+class TestRouteDistance(BaseTestCase):
+
+    """Unittests to test distance calculations between nodes"""
+
+    def test_distance_abc(self):
+        """The distance of the route A-B-C."""
+        self.assertEqual(
+            RouteGraph(routes=self.graph).count_full_distance([self.a, self.b,
+                                                               self.c]),
+            9
+        )
+
+    def test_distance_ad(self):
+        """The distance of the route A-D."""
+        self.assertEqual(
+            RouteGraph(routes=self.graph).count_full_distance([self.a,
+                                                               self.d]),
+            5
+        )
+
+    def test_distance_adc(self):
+        """The distance of the route A-D-C."""
+        self.assertEqual(
+            RouteGraph(routes=self.graph).count_full_distance((self.a, self.d,
+                                                               self.c)),
+            13
+        )
+
+    def test_distance_aebcd(self):
+        """The distance of the route A-E-B-C-D."""
+        self.assertEqual(
+            RouteGraph(routes=self.graph).count_full_distance((self.a, self.e,
+                                                               self.b, self.c,
+                                                               self.d)),
+            22
+        )
+
+    def test_distance_aed(self):
+        """The distance of the route A-E-D."""
+        self.assertEqual(
+            RouteGraph(routes=self.graph).count_full_distance((self.a, self.e,
+                                                               self.d)),
+            "NO SUCH ROUTE"
+        )
+
+
+class TestNumberOfTripsWithStops(BaseTestCase):
+
+    """Unittests for counting number of trips with
+        given max or exact number of stops"""
+
+    def test_number_of_trips_c_to_c_max(self):
+        """The number of trips starting at C and ending at C
+        with a maximum of 3 stops.
+        In the sample data below, there are two such trips: C-D-C (2
+            stops). and C-E-B-C (3 stops)."""
+        self.assertEqual(
+            RouteGraph(routes=self.graph).count_trips_max_stops(
+                self.c, self.c, max_stops=3
+            ),
+            2
+        )
+
+    def test_number_of_trips_a_to_c_exact(self):
+        """The number of trips starting at A and ending at C
+        with exactly 4 stops.
+            In the sample data below, there are three such trips:
+            A to C (via B,C,D); A
+            to C (via D,C,D); and A to C (via D,E,B)."""
+        self.assertEqual(
+            RouteGraph(routes=self.graph).count_trips_fixed_stops(
+                self.a, self.c, fixed_stops=4
+            ),
+            3
+        )
+
+
+class TestShortestRoute(BaseTestCase):
+
+    """Unittests for calculating the shortest
+        route in terms of distance between two points"""
+
+    def test_AC(self):
+        """The length of the shortest route (in terms of distance to travel)
+        from A to C"""
+        self.assertEqual(
+            RouteGraph(routes=self.graph).shortest_route(
+                self.a, self.c
+            ),
+            9
+        )
+
+    def test_BB(self):
+        """The length of the shortest route (in terms of distance to travel)
+        from B to B"""
+        self.assertEqual(
+            RouteGraph(routes=self.graph).shortest_route(
+                self.b, self.b
+            ),
+            9
+        )
 
 
 class TestDifferentRoutesGivenMaxDistance(BaseTestCase):
